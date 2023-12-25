543829286814569ea17d9ca71395548272302e56
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-12-25 13:46:16 +0530

    Add: comment on rust macro

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,4 +1,6 @@
 fn main() {
     // The main function takes no parameters and returns nothing
     println!("Hello World!");
+    // Using a ! means that you are calling a macro and not a function
+    // To be discussed in detail later on.
 }
