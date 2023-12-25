5ca57a8ce3cfc83e713c5b036fcb79ab028ac927
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-12-25 13:41:29 +0530

    Trying out cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/rk/workspace/github.com/IAmRiteshKoushik/tut/rustup-rk/hello_cargo/target/debug/hello_cargo: /home/rk/workspace/github.com/IAmRiteshKoushik/tut/rustup-rk/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/rk/workspace/github.com/IAmRiteshKoushik/tut/rustup-rk/hello_cargo/target/release/hello_cargo: /home/rk/workspace/github.com/IAmRiteshKoushik/tut/rustup-rk/hello_cargo/src/main.rs
