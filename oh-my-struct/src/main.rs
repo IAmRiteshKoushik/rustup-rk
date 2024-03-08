8e53359220c67a1c89adc836e85b6c6541ab6a66
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-08 14:47:47 +0530

    Learn structs in Rust

---
+++ oh-my-struct/src/main.rs
@@ -0,0 +1,60 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+// Tuple structs (without names fields)
+// This is useful when you need to reuse a tuple's data structure 
+struct Color(i32, i32, i32); // RGB values
+struct Point(i32, i32, i32); // x, y, z :: Coordinates
+
+// Unit-Like Structs (without any fields)
+// These are used to implement traits which will be discussed later
+struct AlwaysEqual;
+
+
+fn main() {
+    let subject = AlwaysEqual;
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    // --snip--
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+    
+    // This is a verbose way of doing it
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("another@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+
+    // We can user struct update syntax
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+
+        // Explicit initialization (prefer the next method)
+        // username: username,
+        // email: email,
+
+        // Rust suggests you use shorthand declaration if your field name 
+        // is same as parameter name in function
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
---
+++ oh-my-struct/target/debug/deps/oh_my_struct-454630e19504ecde.d
@@ -0,0 +1,5 @@
+/home/rk/workspace/github.com/IAmRiteshKoushik/tut/rustup-rk/oh-my-struct/target/debug/deps/liboh_my_struct-454630e19504ecde.rmeta: src/main.rs
+
+/home/rk/workspace/github.com/IAmRiteshKoushik/tut/rustup-rk/oh-my-struct/target/debug/deps/oh_my_struct-454630e19504ecde.d: src/main.rs
+
+src/main.rs:
